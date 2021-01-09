5f31a0947a2d85b8ab90145a5f8712bfbc826363
XinyeBai4 <53057203+XinyeBai4@users.noreply.github.com>
2021-01-09 16:32:05 -0500

    first react component

--- ppmtool-react-client/src/App.js
+++ ppmtool-react-client/src/App.js
@@ -1,26 +1,37 @@
-import logo from './logo.svg';
 import './App.css';
+import React, { Component } from 'react';
+import Dashboard from './components/Dashboard';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <h1 className='App-title'>Welcom to PPMTool</h1>
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+// function App() {
+//   return (
+//     <div className="App">
+//       <header className="App-header">
+//         <img src={logo} className="App-logo" alt="logo" />
+//         <h1 className='App-title'>Welcom to PPMTool</h1>
+//         <p>
+//           Edit <code>src/App.js</code> and save to reload.
+//         </p>
+//         <a
+//           className="App-link"
+//           href="https://reactjs.org"
+//           target="_blank"
+//           rel="noopener noreferrer"
+//         >
+//           Learn React
+//         </a>
+//       </header>
+//     </div>
+//   );
+// }
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+      <Dashboard />
+      </div>
+    );
+  }
 }
 
 export default App;
---
+++ ppmtool-react-client/src/components/Dashboard.js
@@ -0,0 +1,10 @@
+import React, { Component } from 'react'
+
+export default class Dashboard extends Component {
+    render() {
+        return (
+            <h1>Welcom to the Dashboard</h1>
+        );
+    }
+}
+
