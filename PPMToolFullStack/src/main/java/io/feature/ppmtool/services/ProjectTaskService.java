2566cc4423d61ed33dcd6015160d404b5af9008a
XinyeBai4 <53057203+XinyeBai4@users.noreply.github.com>
2021-01-12 15:18:47 -0500

    Persist project task

--- PPMToolFullStack/src/main/java/io/feature/ppmtool/services/ProjectTaskService.java
+++ PPMToolFullStack/src/main/java/io/feature/ppmtool/services/ProjectTaskService.java
@@ -1,5 +1,7 @@
 package io.feature.ppmtool.services;
 
+import io.feature.ppmtool.domain.Backlog;
+import io.feature.ppmtool.domain.ProjectTask;
 import io.feature.ppmtool.repositories.BacklogRepository;
 import io.feature.ppmtool.repositories.ProjectRepository;
 import io.feature.ppmtool.repositories.ProjectTaskRepository;
@@ -15,14 +17,32 @@ public class ProjectTaskService {
     @Autowired
     private ProjectTaskRepository projectTaskRepository;
 
-    public ProjectTask addProjectTask() {
+    public ProjectTask addProjectTask(String projectIdentifier, ProjectTask projectTask) {
+        // Exception : Project not found
+
         // PTs to be a specific project, project != null => BL exists
+        Backlog backlog = backlogRepository.findByProjectIdentifier(projectIdentifier);
         // set the BL to PT
+        projectTask.setBacklog(backlog);
 
         // we want our project sequence like this: IDPRO-1 IDPRO-2
+        Integer BacklogSequence = backlog.getPTSequence();
         // Update the BL sequence
+        BacklogSequence++;
+
+        // Add sequence to project task
+        projectTask.setProjectSequence(projectIdentifier + "-" + BacklogSequence);
+        projectTask.setProjectIdentifier(projectIdentifier);
 
         // Initial priority when priority is null
+//        if (projectTask.getPriority() == 0 || projectTask.getPriority() == null) {
+//            projectTask.setPriority(3);
+//        }
         // Initial status when status is null
+        if (projectTask.getStatus() == "" || projectTask.getStatus() == null) {
+            projectTask.setStatus("TO_DO");
+        }
+
+        return projectTaskRepository.save(projectTask);
     }
 }
