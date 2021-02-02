002593eff25aa1c2357edb496c547472be0a4bc1
XinyeBai4 <53057203+XinyeBai4@users.noreply.github.com>
2021-02-01 19:53:35 -0500

    Find project task by sequence

--- PPMToolFullStack/src/main/java/io/feature/ppmtool/exceptions/ProjectNotFoundException.java
+++ PPMToolFullStack/src/main/java/io/feature/ppmtool/exceptions/ProjectNotFoundException.java
@@ -6,8 +6,8 @@ import org.springframework.web.bind.annotation.ResponseStatus;
 @ResponseStatus(HttpStatus.BAD_REQUEST)
 public class ProjectNotFoundException extends RuntimeException{
 
-	public ProjectNotFoundException(String arg0) {
-		super(arg0);
+	public ProjectNotFoundException(String message) {
+		super(message);
 		// TODO Auto-generated constructor stub
 	}
 	
